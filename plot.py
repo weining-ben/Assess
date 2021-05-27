5c3d2c1939ecf354c9730c94131ca96937befe13
weining <weining_ben@outlook.com>
2021-05-27 12:38:41 +0200

    rajouter le nom des courbes sur le graph de droite dans fonction utilities

--- plot.py
+++ plot.py
@@ -42,7 +42,8 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         b = dictionary[func]['b']
                         c = dictionary[func]['c']
                         ax.plot(x, funcexp(x, a, b, c), '#401539',
-                                label="Exp Fitted Curve")
+                                label="Exp")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func == 'quad':
                 if plot_all or 'quadratic' in liste:
@@ -50,7 +51,8 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         b = dictionary[func]['b']
                         c = dictionary[func]['c']
                         ax.plot(x, funcquad(x, a, b, c), '#458C8C',
-                                label="Quad Fitted Curve")
+                                label="Quad")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func == 'pow':
                 if plot_all or 'power' in liste:
@@ -58,7 +60,8 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         b = dictionary[func]['b']
                         c = dictionary[func]['c']
                         ax.plot(x, funcpuis(x, a, b, c), '#6DA63C',
-                                label="Pow Fitted Curve")
+                                label="Pow")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func == 'log':
                 if plot_all or 'logarithmic' in liste:
@@ -67,13 +70,15 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         c = dictionary[func]['c']
                         d = dictionary[func]['d']
                         ax.plot(x, funclog(x, a, b, c, d),
-                                '#D9585A', label="Log Fitted Curve")
+                                '#D9585A', label="Log")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func == 'lin':
                 if plot_all or 'linear' in liste:
                         a = dictionary[func]['a']
                         b = dictionary[func]['b']
-                        ax.plot(x, funclin(x, a, b), '#D9B504', label="Lin Fitted Curve")
+                        ax.plot(x, funclin(x, a, b), '#D9B504', label="Lin")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func =='expo-power':
                 if plot_all or 'exponential-power' in liste:
@@ -81,7 +86,8 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         b = dictionary[func]['b']
                         c = dictionary[func]['c']
                         ax.plot(x, funcexpopower(x, a, b, c),
-                                '#26C4EC', label="Expo-Power Fitted Curve")
+                                '#26C4EC', label="Expo-Power")
+                        ax.legend(loc='best',fontsize=6)
 
     else: 
         plt.xlim(0,max_ - min_)
@@ -92,7 +98,8 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         b = dictionary[func]['b']
                         c = dictionary[func]['c']
                         ax.plot(x, funcexp(xneg, a, b, c), '#401539',
-                                label="Exp Fitted Curve")
+                                label="Exp")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func == 'quad':
                 if plot_all or 'quadratic' in liste:
@@ -100,7 +107,8 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         b = dictionary[func]['b']
                         c = dictionary[func]['c']
                         ax.plot(x, funcquad(xneg, a, b, c), '#458C8C',
-                                label="Quad Fitted Curve")
+                                label="Quad")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func == 'pow':
                 if plot_all or 'power' in liste:
@@ -108,7 +116,8 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         b = dictionary[func]['b']
                         c = dictionary[func]['c']
                         ax.plot(x, funcpuis(xneg, a, b, c), '#6DA63C',
-                                label="Pow Fitted Curve")
+                                label="Pow")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func == 'log':
                 if plot_all or 'logarithmic' in liste:
@@ -117,13 +126,15 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         c = dictionary[func]['c']
                         d = dictionary[func]['d']
                         ax.plot(x, funclog(xneg, a, b, c, d),
-                                '#D9585A', label="Log Fitted Curve")
+                                '#D9585A', label="Log")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func == 'lin':
                 if plot_all or 'linear' in liste:
                         a = dictionary[func]['a']
                         b = dictionary[func]['b']
-                        ax.plot(x, funclin(xneg, a, b), '#D9B504', label="Lin Fitted Curve")
+                        ax.plot(x, funclin(xneg, a, b), '#D9B504', label="Lin")
+                        ax.legend(loc='best',fontsize=6)
 
             elif func =='expo-power':
                 if plot_all or 'exponential-power' in liste:
@@ -131,7 +142,8 @@ def generate_svg_plot(dictionary, min_, max_, liste_cord, width,liste):
                         b = dictionary[func]['b']
                         c = dictionary[func]['c']
                         ax.plot(x, funcexpopower(xneg, a, b, c),
-                                '#26C4EC', label="Expo-Power Fitted Curve")
+                                '#26C4EC', label="Expo-Powe")
+                        ax.legend(loc='best',fontsize=6)
 
 
     ax.scatter(x1, y1, c='k', label="Original Data")
